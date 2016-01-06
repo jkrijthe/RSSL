0b0c8ef72731020cb756939110c258af5b90e700
Jesse Krijthe <jkrijthe@gmail.com>
2016-01-06 15:30:56 +0100

    Sets more sensible tolerance levels for tests of svmlin

--- tests/testthat/test-svmlin.R
+++ tests/testthat/test-svmlin.R
@@ -9,7 +9,7 @@ t_svmlin <- svmlin(A,factor(rep(c(1,-1),4)),Xu=NULL, lambda = 1,algorithm=1);
 t_liblin <- LiblineaR(as.matrix(A),rep(c(1,-1),4),type=1,epsilon=0.0001,cost=0.5)
 
 test_that("svmlin and LiblineaR give the same results", {
-  expect_equal(t_svmlin$Weights[-1], t_liblin$W[-length(t_liblin$W)])
-  expect_equal(decisionvalues.svmlin(t_svmlin,A),predict(t_liblin,as.matrix(A),decisionValues = TRUE)$decisionValues[,1])
-  expect_equal(predict(t_svmlin,A),as.factor(predict(t_liblin,as.matrix(A))$predictions))
+  expect_equal(t_svmlin$Weights[-1], t_liblin$W[-length(t_liblin$W)],tolerance=10e-4)
+  expect_equal(decisionvalues.svmlin(t_svmlin,A),predict(t_liblin,as.matrix(A),decisionValues = TRUE)$decisionValues[,1],tolerance=10e-4)
+  expect_equal(predict(t_svmlin,A),as.factor(predict(t_liblin,as.matrix(A))$predictions),tolerance=10e-4)
 })
