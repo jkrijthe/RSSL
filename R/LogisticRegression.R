b17c3bc10f049e2532e904748c73c8e4edbf2917
Jesse Krijthe <jkrijthe@gmail.com>
2016-01-19 13:01:43 +0100

    Improvements to LogisticRegression and its unit tests

--- R/LogisticRegression.R
+++ R/LogisticRegression.R
@@ -1,10 +1,15 @@
 #' @include Classifier.R
 setClass("LogisticRegression", 
-         representation(w="numeric",intercept="ANY",scaling="ANY"), 
-         prototype(name="LogisticRegression"), 
+         representation(w="numeric",
+                        intercept="ANY",
+                        scaling="ANY",
+                        opt_result="ANY"), 
+         prototype(name="LogisticRegression",opt_result=NULL), 
          contains="Classifier")
 
-LogisticRegression <- function(X, y, lambda=0, intercept=TRUE, scale=FALSE, init=NA,x_center=FALSE, ...) {
+#' (Regularized) Logistic Regression implementation used for comparison with semi-supervised implementations
+#' @export
+LogisticRegression <- function(X, y, lambda=0, intercept=TRUE, scale=FALSE, init=NA, x_center=FALSE) {
   
   ## Preprocessing to correct datastructures and scaling  
   ModelVariables<-PreProcessing(X,y,scale=scale,intercept=intercept,x_center=x_center)
@@ -16,54 +21,58 @@ LogisticRegression <- function(X, y, lambda=0, intercept=TRUE, scale=FALSE, init
   
   if (length(classnames)!=2) stop("Dataset does not contain 2 classes")
 
-  opt_func <- function(w, X, y) {
-    w <- matrix(w,nrow=ncol(X))
-  
-    # Multiclass:
-    expscore <- cbind(rep(0,nrow(X)), X %*% w) # Numerators of the probability estimates
-  
-    ll <- sum(-log(rowSums(exp(expscore)))) # Denominators of the probability estimates, summed
-  
-    for (c in 1:length(classnames)) {
-      ll <- ll + sum(expscore[y==c,c]) # Sum the numerators for each class
-    }
-
-    return(as.numeric(ll + lambda * w[-1] %*% w[-1]))
-  }
-  
-  opt_grad <- function(w, X, y) {
-    
-    w <- matrix(w,nrow=ncol(X))
-    
-    # Two-class
-    #t(y-(1-1/(1+exp(X %*% w)))) %*% X
-    
-    # Multi-class
-    expscore <- cbind(rep(0,nrow(X)), X %*% w) # Numerators of the probability estimates    
-    
-    for (c in 2:length(classnames)) {
-      w[,c-1] <- matrix(colSums(X[y==c,,drop=FALSE]), ncol(X),1) - (t(X) %*% (exp(expscore[,c]) / rowSums(exp(expscore))))
-    }
-    as.vector(w)
-  }
-  
+  # Initialization
   if (is.na(init[1])) {
     w <- rep(0.0,ncol(X)*(length(classnames)-1))
   } else {
     w<-init
   }
   
-  opt_result <- optim(w, fn=opt_func,gr=opt_grad, X=X, y=y, method="BFGS", control=list(fnscale=-1))
-  # w<-as.numeric(opt_result[1,1:length(w)])
+  # Optimization
+  opt_result <- optim(w, 
+                      fn=loss_logisticregression,
+                      gr=grad_logisticregression, 
+                      X=X, y=y, lambda=lambda,
+                      classnames=classnames,
+                      method="BFGS", 
+                      control=list(fnscale=-1))
+  
   w<-opt_result$par
-  return(new("LogisticRegression", modelform=modelform, classnames=classnames, w=w,intercept=intercept,scaling=scaling))
+  
+  return(new("LogisticRegression", 
+             modelform=modelform, 
+             classnames=classnames, 
+             intercept=intercept,
+             scaling=scaling,
+             w=w,
+             opt_result=opt_result))
   
 }
 
-# Train the Logistic Regression using R's standard glm function
-LogisticRegressionFast<-function(modelform,D) {
-  object<-glm(formula = modelform, family = binomial("logit"), data = D)
-  return(new("LogisticRegression", modelform=modelform, D=D, w=object$coefficients))
+#' Train the Logistic Regression using R's standard glm function
+#' @export
+LogisticRegressionFast <- function(X, y, lambda=0, intercept=TRUE, scale=FALSE, init=NA, x_center=FALSE) {
+  
+  stopifnot(lambda==0)
+  
+  ModelVariables<-PreProcessing(X,y,
+                                scale=scale,
+                                intercept=intercept,
+                                x_center=x_center)
+  X<-ModelVariables$X
+  y<-ModelVariables$y
+  scaling<-ModelVariables$scaling
+  classnames<-ModelVariables$classnames
+  modelform<-ModelVariables$modelform
+
+  object <- glm.fit(X, y, family = binomial("logit"),intercept=FALSE)
+
+    return(new("LogisticRegression", 
+             modelform=modelform, 
+             classnames=classnames, 
+             w=object$coefficients,
+             intercept=intercept,
+             scaling=scaling))
 }
 
 # Log likelihood on a new data matrix X and outcome vector y
@@ -96,19 +105,13 @@ setMethod("loss", signature(object="LogisticRegression"), function(object, newda
   # Multiclass:
   expscore <- cbind(rep(0,nrow(X)), X %*% w) # Numerators of the probability estimates
   
-  ll <- sum(-log(rowSums(exp(expscore)))) # Denominators of the probability estimates, summed
+  ll <- -log(rowSums(exp(expscore))) # Denominators of the probability estimates
   
   for (c in 1:length(object@classnames)) {
-    ll <- ll + sum(expscore[y==c,c]) # Sum the numerators for each class
+    ll[y==c] <- ll[y==c] + expscore[y==c,c] # Sum the numerators for each class
   }
   return(-ll)
-})                                                                              
-
-
-#' Log likelihood on the training set                                                                              
-# setMethod("logLik", signature(object="LogisticRegression"), function(object) {
-#   loss(object,object@D)
-# })
+})
 
 #' @rdname rssl-predict
 #' @aliases predict,LogisticRegression-method  
@@ -116,10 +119,10 @@ setMethod("predict", signature(object="LogisticRegression"), function(object, ne
 ModelVariables<-PreProcessingPredict(object@modelform,newdata,scaling=object@scaling,intercept=object@intercept)
   X<-ModelVariables$X
 
-
   w <- matrix(object@w, nrow=ncol(X))
   expscore <- exp(cbind(rep(0,nrow(X)), X %*% w))
   probabilities <- expscore/rowSums(expscore)
+  
   # If we need to return classes
   classes <- factor(apply(probabilities,1,which.max),levels=1:length(object@classnames), labels=object@classnames)
   if (probs)
@@ -128,8 +131,41 @@ ModelVariables<-PreProcessingPredict(object@modelform,newdata,scaling=object@sca
   } else return(classes)
 })
 
-#' @rdname boundaryplot-methods
-#' @aliases boundaryplot,LogisticRegression-method  
-setMethod("boundaryplot", signature(object="LogisticRegression"), function(object, p) {
-  p+geom_abline(intercept = (-(object@w[1])/object@w[3]), slope = (-object@w[2]/object@w[3]))
+#' @rdname line_coefficients-methods
+#' @aliases line_coefficients,LogisticRegression-method 
+setMethod("line_coefficients", signature(object="LeastSquaresClassifier"), function(object) {
+  return(coefficients_after_scaling(w0=object@w[1]-(0.5-object@y_scale),w=object@w[2:3],scaling=object@scaling))
 })
+
+loss_logisticregression <- function(w, X, y, classnames,lambda) {
+  w <- matrix(w,nrow=ncol(X))
+  
+  # Multiclass:
+  expscore <- cbind(rep(0,nrow(X)), X %*% w) # Numerators of the probability estimates
+  
+  ll <- sum(-log(rowSums(exp(expscore)))) # Denominators of the probability estimates, summed
+  
+  for (c in 1:length(classnames)) {
+    ll <- ll + sum(expscore[y==classnames[c],c]) # Sum the numerators for each class
+  }
+  
+  return(as.numeric(ll - lambda * w[-1] %*% w[-1]))
+}
+
+grad_logisticregression <- function(w, X, y, classnames, lambda) {
+  w <- matrix(w,nrow=ncol(X))
+  
+  # Two-class
+  #t(y-(1-1/(1+exp(X %*% w)))) %*% X
+  
+  # Multi-class
+  expscore <- cbind(rep(0,nrow(X)), X %*% w) # Numerators of the probability estimates    
+  
+  grad <- matrix(w,nrow=ncol(X))
+  for (cu_class in 2:length(classnames)) {
+    grad[,cu_class-1] <- matrix(colSums(X[y==classnames[cu_class],,drop=FALSE]), ncol(X),1) + 
+      - (t(X) %*% (exp(expscore[,cu_class]) / rowSums(exp(expscore))))
+  }
+  
+  as.numeric(grad - 2* lambda* c(0,w[-1,]))
+}
---
+++ tests/testthat/test-LogisticRegression.R
@@ -0,0 +1,75 @@
+context("Logistic Regression")
+
+# Simple dataset used in the tests
+data(testdata)
+modelform <- testdata$modelform
+classname<-all.vars(modelform)[1] 
+D <- testdata$D
+D_test <- testdata$D_test
+X <- testdata$X
+X_u <- testdata$X_u
+y <- testdata$y
+X_test <- testdata$X_test
+y_test <- testdata$y_test
+
+test_that("Same result for different input modalities",{
+  g_mat <- LogisticRegression(X,y)
+  g_df <- LogisticRegression(modelform,D)
+  
+  expect_equal(predict(g_mat,X_test),predict(g_df,D_test))
+  expect_equal(loss(g_mat,X_test,y_test),loss(g_df,D_test))
+})
+
+test_that("Centering input does not change predictions", {
+
+  g_center <- LogisticRegression(X,y,
+                                 x_center=TRUE,lambda=10)
+  g_noncenter <- LogisticRegression(X,y,
+                                    x_center=FALSE,lambda=10)
+  
+  expect_equal(g_center@w[-1],g_noncenter@w[-1],tolerance=10e-4)
+  
+  g_center@opt_result
+  g_noncenter@opt_result
+  expect_equal(loss(g_center,X_test,y_test),
+               loss(g_noncenter,X_test,y_test),
+               tolerance=10e-5)
+  expect_equal(predict(g_center,X_test),
+               predict(g_noncenter,X_test))
+  
+  g_center <- LogisticRegression(X[,1,drop=FALSE],y,x_center=TRUE)
+  g_noncenter <- LogisticRegression(X[,1,drop=FALSE],y,x_center=FALSE)
+  
+  expect_equal(loss(g_center,X_test[,1,drop=FALSE],y_test),
+               loss(g_noncenter,X_test[,1,drop=FALSE],y_test),tolerance=10e-4)
+  
+  expect_equal(predict(g_center,X_test[,1,drop=FALSE]),
+               predict(g_noncenter,X_test[,1,drop=FALSE]))
+})
+
+test_that("Gradient is superficially correct",{
+  library("numDeriv")
+  data(testdata)
+  
+  X <- cbind(1,testdata$X)
+  y <- testdata$y
+  classnames <- levels(y)
+  
+  for (i in 1:10) {
+    w <- rnorm(ncol(X))
+    lambda <- abs(10*rnorm(1))
+    expect_equal(as.numeric(numDeriv::grad(loss_logisticregression,w,X=X,y=y,classnames=classnames,lambda=lambda, method="simple")),
+                 as.numeric(grad_logisticregression(w,X=X,y=y,classnames=classnames,lambda=lambda)),
+                 tolerance=10e-4)
+  }
+})
+
+test_that("LogisticRegression gives same solution as glm",{
+
+  g_fast <- LogisticRegressionFast(X[,1,drop=FALSE],y)
+  g_lr <- LogisticRegression(X[,1,drop=FALSE],y)
+  expect_equal(as.numeric(g_fast@w), g_lr@w,tolerance=10e-4)
+
+  expect_equal(predict(g_fast,X_test[,1,drop=FALSE]),
+               predict(g_lr,X_test[,1,drop=FALSE]))
+})
