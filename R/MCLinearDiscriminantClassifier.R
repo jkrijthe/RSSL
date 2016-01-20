d89f741894364dc1b2b7ab7427f31759b6e5ee65
Jesse Krijthe <jkrijthe@gmail.com>
2016-01-20 15:18:13 +0100

    Small changes to MCLinearDiscriminantAnalysis

--- R/MCLinearDiscriminantClassifier.R
+++ R/MCLinearDiscriminantClassifier.R
@@ -4,27 +4,24 @@ setClass("MCLinearDiscriminantClassifier",
          prototype(name="Moment Constrained Linear Discriminant Classifier"),
          contains="LinearDiscriminantClassifier")
 
-#' Moment Constrained Semi-Supervised Linear Discriminant Analysis.
+#' Moment Constrained Semi-supervised Linear Discriminant Analysis.
 #'
-#' Using an update of the estimated means and covariance proposed by \cite{Loog}. 
-#' Using the method="closedform" option, uses the ad hoc parameter update proposed in \cite{Loog}. The alternative way to minimize this objective function directly has not been implemented yet.
+#' Using an update of the estimated means and covariance proposed in (Loog 2014). 
+#' Using the method="closedform" option, uses the ad hoc parameter update proposed in (Loog 2014). The alternative way to minimize this objective function directly has not been implemented yet.
+#' @references Loog, M., 2014. Semi-supervised linear discriminant analysis through moment-constraint parameter estimation. Pattern Recognition Letters, 37, pp.24-31.
 #'
-#'
-#' @param X Matrix with n objects for m features
-#' @param y factor with labels for n objects
-#' @param X_u Matrix with n objects for m features for unlabeled data
-#' @param method either "ml" or "closedform"
+#' @param method character; Currently only "closedform"
 #' @param prior Matrix (k by 1) of class prior probabilities. If NULL, estimated from data
 #' @inheritParams BaseClassifier
 #' 
 #' @family RSSL classifiers
 #' @export
-MCLinearDiscriminantClassifier <- function(X, y, X_u, method="closedform",prior=NULL, scale=FALSE) {
+MCLinearDiscriminantClassifier <- function(X, y, X_u, method="closedform",prior=NULL, x_center=FALSE, scale=FALSE) {
   ## Preprocessing to correct datastructures and scaling  
-  ModelVariables<-PreProcessing(X=X,y=y,X_u=X_u,scale=scale,intercept=FALSE)
-  X<-ModelVariables$X
-  X_u<-ModelVariables$X_u
-  y<-ModelVariables$y
+  ModelVariables<-PreProcessing(X=X,y=y,X_u=X_u,scale=scale,intercept=FALSE,x_center=x_center)
+  X <- ModelVariables$X
+  X_u <- ModelVariables$X_u
+  y <- ModelVariables$y
   scaling<-ModelVariables$scaling
   classnames<-ModelVariables$classnames
   modelform<-ModelVariables$modelform
@@ -34,7 +31,7 @@ MCLinearDiscriminantClassifier <- function(X, y, X_u, method="closedform",prior=
   if (method=="closedform") {
     
     #Set priors if not set by user
-    if (is.null(prior)) prior<-matrix(colMeans(Y),2,1)
+    if (is.null(prior)) prior <- matrix(colMeans(Y),2,1)
     
     #Calculate means for classes
     means<-t((t(X) %*% Y))/(colSums(Y))
@@ -46,44 +43,30 @@ MCLinearDiscriminantClassifier <- function(X, y, X_u, method="closedform",prior=
       sigma<-sigma+sigma.classes[[i]]*prior[i]
     }
     
-    
     T.labeled<-cov_ml(X)
     T.all<-cov_ml(rbind(X,X_u))
     m.labeled<-colMeans(X)
     m.all<-colMeans(rbind(X,X_u))
     
-    
     matrixsqrt <- function(X) {
-      decomposition<-eigen(X)
+      decomposition <- eigen(X)
       decomposition$vectors %*% diag(sqrt(decomposition$values)) %*% t(decomposition$vectors)
     }
-
     
-    sigma <- matrixsqrt(T.all) %*% ginv(matrixsqrt(T.labeled)) %*% sigma %*% ginv(matrixsqrt(T.labeled)) %*% matrixsqrt(T.all)
-    means <- t(matrixsqrt(T.all) %*% ginv(matrixsqrt(T.labeled)) %*% t(means-matrix(1,nrow(means),1) %*% m.labeled ) + t(matrix(1,nrow(means),1) %*% m.all)) 
+    means <- t(matrixsqrt(T.all) %*% ginv(matrixsqrt(T.labeled)) %*% t(means-matrix(1,nrow(means),1) %*% m.labeled ) + t(matrix(1,nrow(means),1) %*% m.all))
+    sigma <- ginv(matrixsqrt(T.labeled)) %*% matrixsqrt(T.all) %*%  sigma  %*% matrixsqrt(T.all) %*% ginv(matrixsqrt(T.labeled))
+    
     
     sigma<-lapply(1:ncol(Y),function(c){sigma})
     
   } else if (method=="ml") {
     stop("Not implemented yet")
-#     maxIterations<-10000
-#     eps<-1e-2
-#     stepFactor = 1e-1
-#     # Check for singular covariance matrix
-#     if 
-#     
-#     Ns<-table(y)
-#     
-#     means<-colMeans(Xe) # Initialize means to overal mean
-#     Ns<-table(y)
-#     
-#     #Stores Sums of the samples in each class
-#     # Stores the uncorrected class scatter
-#     
-#     for (iter in 1:maxIterations) {
-#       
-#       
-#     }
   }
-  new("MCLinearDiscriminantClassifier", prior=prior, means=means, sigma=sigma,classnames=classnames,scaling=scaling, modelform=modelform)
+  new("MCLinearDiscriminantClassifier", 
+      prior=prior, 
+      means=means, 
+      sigma=sigma,
+      classnames=classnames,
+      scaling=scaling, 
+      modelform=modelform)
 }
---
+++ tests/testthat/test-MCLinearDiscriminantClassifier.R
@@ -0,0 +1,61 @@
+context("Moment Constrained Linear Discriminant Classifier")
+
+# Simple dataset used in the tests
+data(testdata)
+modelform <- testdata$modelform
+classname<-all.vars(modelform)[1] 
+D <- testdata$D
+D_test <- testdata$D_test
+X <- testdata$X
+X_u <- testdata$X_u
+y <- testdata$y
+X_test <- testdata$X_test
+y_test <- testdata$y_test
+
+test_that("Formula and matrix formulation give same results",{
+  g_matrix <- MCLinearDiscriminantClassifier(X,y,X_u)
+  g_model <- MCLinearDiscriminantClassifier(modelform, D)
+  
+  expect_equal(predict(g_matrix,X_test),
+               predict(g_model,D_test))
+  expect_equal(loss(g_matrix, X_test, y_test),
+               loss(g_model, D_test))
+  expect_equal(g_matrix@classnames,g_model@classnames)
+})
+
+test_that("Adding the labeled data again does not help",{
+  g_semi <- MCLinearDiscriminantClassifier(X, y, X)
+  g_sup <- LinearDiscriminantClassifier(X, y)
+  
+  expect_equal(predict(g_semi,X_test), predict(g_sup,X_test))
+  expect_equal(posterior(g_semi,X_test), posterior(g_sup,X_test))
+  expect_equal(g_semi@classnames,g_sup@classnames) # Class names the same?
+})
+
+test_that("Centering has no effect",{
+  g_1 <- MCLinearDiscriminantClassifier(X, y, X_u, x_center=TRUE)
+  g_2 <- MCLinearDiscriminantClassifier(X, y, X_u, x_center=FALSE)
+  
+  expect_equal(predict(g_1,X_test), predict(g_2,X_test))
+  expect_equal(posterior(g_1,X_test), posterior(g_2,X_test))
+  expect_equal(g_1@classnames,g_2@classnames) # Class names the same?
+  expect_equal(line_coefficients(g_1), line_coefficients(g_2))
+})
+
+# test_that("Scaling has no effect",{
+#   g_1 <- MCLinearDiscriminantClassifier(X, y, X_test, scale=TRUE)
+#   g_2 <- MCLinearDiscriminantClassifier(X, y, X_test, scale=FALSE,x_center=TRUE)
+#   
+#   g_1@sigma[[1]] %*% diag(g_1@scaling@scale)
+#   g_2@sigma[[1]]
+#   
+#   g_3 <- LinearDiscriminantClassifier(X, y, scale=TRUE)
+#   g_4 <- LinearDiscriminantClassifier(X, y, scale=FALSE)
+#   expect_equal(line_coefficients(g_3), line_coefficients(g_4))
+#   
+#   expect_equal(predict(g_1,X_test), predict(g_2,X_test))
+#   expect_equal(posterior(g_1,X_test), posterior(g_2,X_test))
+#   max(abs(posterior(g_1,X_test)-posterior(g_2,X_test)))
+#   expect_equal(g_1@classnames,g_2@classnames)
+#   expect_equal(line_coefficients(g_1), line_coefficients(g_2))
+# })
