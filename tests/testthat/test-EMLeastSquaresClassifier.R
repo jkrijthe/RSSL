653725331f48f048e2e723b7e7cc9172431a865d
Jesse Krijthe <jkrijthe@gmail.com>
2016-08-03 15:05:01 +0200

    Adds tests for EMLeastSquares

--- tests/testthat/test-EMLeastSquaresClassifier.R
+++ tests/testthat/test-EMLeastSquaresClassifier.R
@@ -33,3 +33,77 @@ test_that("Different settings return the same loss",{
   
   expect_equal(loss(g_3,X_test,y_test),loss(g_4,X_test,y_test),tolerance=10e-6)
 })
+
+test_that("Hard label EM and self-learning give the same result", {
+  
+  data <- generate2ClassGaussian(200,d=2, expected=FALSE)
+  data <- add_missinglabels_mar(data, Class~., prob=0.9)
+  data_test <- generate2ClassGaussian(200,d=2, expected=FALSE)
+  
+  g_block <-  EMLeastSquaresClassifier(Class~.,data,method="block",objective="responsibility",init="supervised",save_all=TRUE)
+  
+  g_self <- SelfLearning(Class~.,data,method=LeastSquaresClassifier)
+  
+  expect_equal(loss(g_block,data_test), loss(g_self,data_test))
+  expect_equal(g_block@theta, g_self@model@theta)
+})
+
+test_that("Gradient superficially correct",{
+  library("numDeriv")
+  
+  data(testdata)
+  
+  X <- cbind(1,testdata$X)
+  X_u <- cbind(1,testdata$X_u)
+  Xe <- rbind(X,X_u)
+  Y <- model.matrix(~y-1,data.frame(y=testdata$y))[,1,drop=FALSE]
+  
+  for (i in 1:100) {
+    w <- c(rnorm(ncol(X)),runif(nrow(X_u)))
+    
+    grad_num <- as.numeric(
+      numDeriv::grad(
+        loss_minmin_lsy,
+        w, Xe=Xe, Y=Y, X_u=X_u,
+        method="simple")
+    )
+    
+    grad_exact <- as.numeric(
+      gradient_minmin_lsy(
+        w, Xe=Xe, Y=Y, X_u=X_u)
+    )
+    
+    expect_equal(grad_num,grad_exact,
+                 tolerance=10e-4)
+  }
+})
+
+test_that("Gradient superficially correct",{
+  library("numDeriv")
+  
+  data(testdata)
+  
+  X <- cbind(1,testdata$X)
+  X_u <- cbind(1,testdata$X_u)
+  Xe <- rbind(X,X_u)
+  Y <- model.matrix(~y-1,data.frame(y=testdata$y))[,1,drop=FALSE]
+  
+  for (i in 1:100) {
+    w <- c(rnorm(ncol(X)),runif(nrow(X_u)))
+    
+    grad_num <- as.numeric(
+      numDeriv::grad(
+        loss_minmin_lsq,
+        w, Xe=Xe, Y=Y, X_u=X_u,X=X,
+        method="simple")
+    )
+    
+    grad_exact <- as.numeric(
+      gradient_minmin_lsq(
+        w, Xe=Xe, Y=Y, X_u=X_u,X=X)
+    )
+    
+    expect_equal(grad_num,grad_exact,
+                 tolerance=10e-4)
+  }
+})
