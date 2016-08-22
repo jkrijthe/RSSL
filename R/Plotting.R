2be84d4ce6c9ff427dc46fe926d3d2bafea4d22b
Jesse Krijthe <jkrijthe@gmail.com>
2016-08-22 13:41:00 +0200

    Adds stat_classifier to plot classifiers in 2D

--- R/Plotting.R
+++ R/Plotting.R
@@ -23,7 +23,7 @@ coefficients_after_scaling <- function(w0,w,scaling) {
 #' @param ... List of trained classifiers
 #' @param show_guide logical (default: TRUE); Show legend
 #' @export
-geom_classifier <- function(...,show_guide=TRUE) {
+geom_linearclassifier <- function(...,show_guide=TRUE) {
   classifiers <- list(...)
   alt_names <-  eval(substitute(alist(...)))
   if (is.null(names(classifiers))) names(classifiers) <- alt_names
@@ -37,11 +37,120 @@ geom_classifier <- function(...,show_guide=TRUE) {
               data=boundaries,show.legend = show_guide)
 }
 
-#' Plot RSSL responsibilities
-#' @param classifier Classifier; Classifier to plot the responsibilities for
-#' @param data data.frame; Data corresponding to the coordinates of the unlabeled objects
+
+StatClassifier <- 
+  ggproto("StatClassifier", Stat, 
+          required_aes = c("x","y","z"),
+          
+          setup_params = function(data, params) {
+            params
+          },
+          
+          compute_group = function(data, scales, classifiers, breaks, precision, brute_force) {
+            if (all(data$group>1)) return(NULL)
+            out <- lapply(classifiers, function(x) {
+              if (hasMethod(line_coefficients,class(x)) && !brute_force) {
+                coef <- line_coefficients(x)
+                data.frame(x=c(scales$x$get_limits(), 
+                               (scales$y$get_limits()-coef$intercept)/coef$slope
+                           ),
+                           y=c(coef$intercept + 
+                                 coef$slope * scales$x$get_limits(),
+                               scales$y$get_limits()
+                           )
+                               ,piece=1,group="1",stringsAsFactors = FALSE)
+              } else {
+                
+                df_contour <- expand.grid(x=seq(scales$x$get_limits()[1],
+                                                scales$x$get_limits()[2],
+                                                length.out = precision),
+                                          y=seq(scales$y$get_limits()[1],
+                                                scales$y$get_limits()[2],
+                                                length.out = precision))
+                
+                df_contour_mat <- df_contour
+                if (!is.null(x@modelform)) {
+                  colnames(df_contour) <- attr(terms(x@modelform),
+                                               "term.labels")
+                } else {
+                  df_contour <- as.matrix(df_contour)
+                }
+                
+                df_contour_mat$z <- decisionvalues(x,df_contour) - 
+                  ifelse(.hasSlot(x,"threshold"),x@threshold,0)
+                colnames(df_contour_mat) <- c("x","y","z")
+                out <- ggplot2:::contour_lines(df_contour_mat,breaks=0,complete=TRUE) %>% 
+                  mutate(group=as.character(data$group[1]))
+                out$level <- NULL
+                out
+              }
+            }) %>% 
+              bind_rows(.id="classifier")  %>% 
+              mutate(piece=paste(classifier,piece,sep="-")) %>% 
+              mutate(group=as.integer(factor(paste(classifier,group,sep="-")))) %>% 
+              mutate(group=piece)
+            
+            out
+          }  
+  )
+
+#' Plot RSSL classifier boundaries
+#' 
+#' @examples
+#' library(RSSL)
+#' library(ggplot2)
+#' library(dplyr)
+#' 
+#' df <- generateCrescentMoon(200)
+#' 
+#' g_svm <- SVM(Class~.,df,kernel = kernlab::rbfdot(sigma = 1))
+#' g_ls <- LeastSquaresClassifier(Class~.,df)
+#' g_nm <- NearestMeanClassifier(Class~.,df)
+#' 
+#' df %>% 
+#'   ggplot(aes(x=X1,y=X2,color=Class,shape=Class)) +
+#'   geom_point(size=3) +
+#'   coord_equal() +
+#'   scale_x_continuous(limits=c(-20,20), expand=c(0,0)) +
+#'   scale_y_continuous(limits=c(-20,20), expand=c(0,0)) +
+#'   stat_classifier(aes(linetype=..classifier..),
+#'                   color="black", precision=50,
+#'                   classifiers=list("SVM"=g_svm,"NM"=g_nm,"LS"=g_ls)
+#'   )
+#' @param mapping aes; aesthetic mapping
+#' @param data data.frame; data to be diplayed
+#' @param inherit.aes logical; If FALSE, overrides the default aesthetics
+#' @param breaks double; decision value for which to plot the boundary
+#' @param precision integer; grid size to sketch classification boundary
+#' @param brute_force logical; If TRUE, uses numerical estimation even for linear classifiers
+#' @param classifiers List of Classifier objects to plot
+#' @param show.legend logical; Whether this layer should be included in the legend
+#' @param ... Additional parameters passed to geom
 #' @export
-geom_responsibilities <- function(classifier,data) {
-  data <- data.frame(data,Responsibility=classifier@responsibilities)
-  geom_point(aes_string(fill="Responsibility"),shape=21,size=4,color="white", data=data) 
+stat_classifier <- function(mapping = NULL, data = NULL, show.legend = NA,
+                            inherit.aes = TRUE, breaks=0, precision=50, 
+                            brute_force=FALSE, classifiers=classifiers,
+                            ...) {
+  
+  if (is.null(names(classifiers))) names(classifiers) <- lapply(classifiers,function(c){c@name})
+  
+  layer(
+    stat = StatClassifier, data = data, mapping = mapping, geom = GeomContour, 
+    position = "identity", show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(classifiers=classifiers, na.rm = TRUE, breaks=breaks,precision=precision, brute_force=brute_force,...)
+  ) 
+}
+
+#' Plot RSSL classifier boundary (deprecated)
+#' 
+#' Deprecated: Use stat_classifier to plot classification boundaries
+#' 
+#' @param ... List of trained classifiers
+#' @param show_guide logical (default: TRUE); Show legend
+#' @export
+geom_classifier <- function(...,show_guide=TRUE) {
+  classifiers <- list(...)
+  alt_names <-  eval(substitute(alist(...)))
+  if (is.null(names(classifiers))) names(classifiers) <- alt_names
+  stat_classifier(aes_string(linetype="..classifier.."), classifiers=classifiers,color="black")
 }
