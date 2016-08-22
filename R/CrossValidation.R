a379bc70bd0f6149aac2d42edfd175392192b1d2
Jesse Krijthe <jkrijthe@gmail.com>
2016-08-22 13:37:51 +0200

    Updates example

--- R/CrossValidation.R
+++ R/CrossValidation.R
@@ -74,16 +74,37 @@ c.CrossValidation <- function(...) {
 #' X <- model.matrix(Species~.-1,data=iris)
 #' y <- iris$Species
 #' 
-#' classifiers <- list("LS"=function(X,y,X_u,y_u) {LeastSquaresClassifier(X,y,lambda=0)}, 
-#'                     "RLS"=function(X,y,X_u,y_u) {LeastSquaresClassifier(X,y,lambda=10)})
+#' classifiers <- list("LS"=function(X,y,X_u,y_u) {
+#'   LeastSquaresClassifier(X,y,lambda=0)}, 
+#'   "EM"=function(X,y,X_u,y_u) {
+#'     SelfLearning(X,y,X_u,
+#'                  method=LeastSquaresClassifier)}
+#' )
+#' 
 #' measures <- list("Accuracy" =  measure_accuracy,
 #'                  "Loss" = measure_losstest,
 #'                  "Loss labeled" = measure_losslab,
 #'                  "Loss Lab+Unlab" = measure_losstrain
-#'                  )
-#' lc <- CrossValidationSSL(X,y,classifiers=classifiers,measures=measures)
-#' print(lc)
-#' plot(lc)
+#' )
+#' 
+#' # Cross-validation making sure test folds are non-overlapping
+#' cvresults1 <- CrossValidationSSL(X,y, 
+#'                                  classifiers=classifiers, 
+#'                                  measures=measures,
+#'                                  leaveout="test", k=10,
+#'                                  repeats = 2,n_labeled = 10)
+#' print(cvresults1)
+#' plot(cvresults1)
+#' 
+#' # Cross-validation making sure labeled sets are non-overlapping
+#' cvresults2 <- CrossValidationSSL(X,y, 
+#'                                  classifiers=classifiers, 
+#'                                  measures=measures,
+#'                                  leaveout="labeled", k=10,
+#'                                  repeats = 2,n_labeled = 10,
+#'                                  prop_unlabeled=0.5)
+#' print(cvresults2)
+#' plot(cvresults2)
 #' 
 #' @export
 CrossValidationSSL <- function(X, y,...) {
